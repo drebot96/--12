a58c5f852255f05f89add0258fd57772ec645fa7
drebot96 <andriy.drebot.96@gmail.com>
2020-07-14 18:53:23 +0300

    dz12

---
+++ script.js
@@ -0,0 +1,104 @@
+//alert('Привет мир');                           1.Завдання
+
+
+
+/*let a = 3;                                     2.Завдання
+let b = 4;
+
+let sum = a + b;
+console.log(sum);
+
+let sam = a + 2;
+console.log(sam);
+
+let sem = a * b;
+console.log(sem);
+
+let sim = a / b;
+console.log(sim);
+
+let slim = a % b;
+console.log(slim);
+
+let seem = a ** b;
+console.log(seem); */ 
+
+
+        
+    
+/* const a = +prompt("Перше");                  3. Завдання
+    
+const b = +prompt("Друге");
+    
+const result = (a / b);
+
+console.log(result); */
+
+
+
+
+/* const a = +prompt("Число");                  4. Завдання
+
+const b = 10
+
+const result = (a === b );
+
+console.log(result); */             
+  
+
+
+
+/* const height = 23;                           5. Завдання
+
+const width = 10;
+
+const s = (height * width);
+
+console.log(s); */          
+  
+
+
+
+/* const name = 'Andriy';                       6. Завдання
+const surname = 'Drebot';
+const city = 'Bogorodhchany';
+const country = 'Ukraine';
+
+const phrase = `Привет, меня зовут ${name} ${surname}, я проживаю в стране ${country}, город - ${city}`;
+console.log(phrase); */
+
+
+
+
+
+/*const name = prompt( 'Ваше імя');             7. Завдання                     
+const surname = prompt('Ваша фамілія');
+const city = prompt('Ваше місто');
+const country = prompt('Ваша країна');
+
+const phrase = `Привет, меня зовут ${name} ${surname}, я проживаю в стране ${country}, город - ${city}`;
+alert(phrase); */ 
+
+ 
+
+/*const x = 3;                                  8. Завдання
+const y = 20;
+
+const result = (72 * y + 22 * x) / 4 + 232;
+console.log(result); */
+
+
+
+/* const x = 16;                                9. Завдання
+const y = 20;
+
+const result = (100 * y / 2 + 5 * x / 5 - 55) * 6;
+console.log(result); */
+
+
+
+/*const r = 3;                                  10. Завдання
+const h = 2;
+
+const v10 = Math.PI * (r ** 2) * h;
+console.log(v10) */
